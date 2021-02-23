36da89e14ac4f3841063583b01eeeef524579e0f
youjin99 <76585610+youjin99@users.noreply.github.com>
2021-02-23 19:16:28 +0900

    Delete fuser directory

--- fuser/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from .models import fuser
-# Register your models here.
-
-
-class FcuserAdmin(admin.ModelAdmin):
-    list_display = ["username","password"] #사용자이름, 비밀번호가 나오도록 설정 
-
-admin.site.register(fuser,FcuserAdmin) 
